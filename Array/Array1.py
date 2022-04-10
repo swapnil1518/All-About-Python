fa4d29966dc1a684f0b9d69ffc1d9798b39e5e95
Swapnil Singh <40495636+swapnil1518@users.noreply.github.com>
2022-04-11 01:44:53 +0530

    Add files via upload

---
+++ Array/Array1.py
@@ -0,0 +1,11 @@
+import array
+store =0
+val = array.array('i',[4,87,23,90,32,23])
+print(val)
+for i in range(7):
+    print(i)   # this print index
+for i in val:
+    print(i)    # this print values of array
+
+val1 = array.array('u',['a','g','c','y','r'])   # to print characters
+print(val1)
